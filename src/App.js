087450aed25a8804b80ee5df4564238a40d776e4
Pedraam Nikzad <pedraamn@gmail.com>
2022-09-04 14:15:51 -0700

    update enter modal, add design drawing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import React, { useState } from 'react';
+import designOverview from '../src/assets/designOverview.png'
 import Modal from 'react-modal/lib/components/Modal';
 import Chessboard from './components/Chessboard';
 
@@ -17,19 +18,23 @@ function App() {
     
     <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '4em'}}>
       <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
-        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', flexDirection: 'column'}}>
+        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', flexDirection: 'column', gap: '0.1em'}}>
           <h2>Pedraam's Go Chess Engine</h2>
-          <p>Hi! Welcome to my Chess Engine written completely in Go! The engine uses the Minimax algorithm with move ordering 
-            and is backed by a bitboard representation. The front-end logic was written in JavaScript under the react framework. 
-            Essentially the way it works is I uploaded all the Chess engine logic into an AWS Lamba function. The front-end 
-            then serializes the board into a string, sends the string as a request to the AWS Lambda Gateway, the engine then 
-            finds the best move from that position, and sends that move back to the front-end as a response.</p>
+          <p>Hi! Welcome to my Chess Engine! The front-end is written in JavaScript(React) while all the engine logic has been written in Go.
+            Rough overview of how it works is the engine logic lives inside an AWS Lambda function. So everytime a human makes a move, the front-end 
+            serializes the board into a string, sends the string as a request to the AWS Lambda Gateway, the engine then 
+            computes the best move from that position, and sends that move back to the front-end as a response.</p>
+          <img src={designOverview} style={{height: '33%', width: '33%'}}/>
           <button style={{
             height: '5em',
-            width: '5em',
+            width: '15em',
 
           }}onClick={closeModal}>Play As White</button>
-          <button>Play As Black (COMING SOON!)</button>
+          <button style={{
+            height: '5em',
+            width: '15em',
+
+          }}>Play As Black (Coming Soon!)</button>
         </div>
       </Modal>
       <a href="https://github.com/pedraamn/React-Chess-Frontend" target="_blank">React Front-end Code</a>
